ae15271618225c12fae772066cfa7a983d8cc4f5
tranvietdz735-lgtm <tranvietdz735@gmail.com>
2026-07-06 03:15:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+const { Client, GatewayIntentBits } = require("discord.js");
 
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.on("ready", () => {
+  console.log(`Đăng nhập thành công: ${client.user.tag}`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === "!ping") {
+    message.reply("🏓 Pong!");
+  }
+});
+
+client.login("DÁN_TOKEN_BOT_CỦA_BẠN_VÀO_ĐÂY");
